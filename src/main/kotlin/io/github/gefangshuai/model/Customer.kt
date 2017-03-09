7bd5c88e7e8c2a6f2f3712c00961bd5e33f70602
葛方帅 <gefangshuai@outlook.com>
2017-03-09 15:47:49 +0800

    ddddd

--- src/main/kotlin/io/github/gefangshuai/model/Customer.kt
+++ src/main/kotlin/io/github/gefangshuai/model/Customer.kt
@@ -7,8 +7,10 @@ import javax.persistence.*
  */
 @Entity
 @Table(name = "customer")
-class Customer constructor(
-        @Id @GeneratedValue(strategy = GenerationType.AUTO) var id: Long = 1,
-        var firstName: String = "",
-        var lastName: String = ""
-        )
+data class Customer (
+        @Id @GeneratedValue(strategy = GenerationType.AUTO) var id: Int?,
+        var firstName: String?,
+        var lastName: String?
+        ){
+    constructor() : this(null, null, null)  // Spring 需要
+}
